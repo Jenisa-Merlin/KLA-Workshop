d53cfaf43407b43dd805bd6f397cf9ef6e669594
Jenisa-Merlin <102733648+Jenisa-Merlin@users.noreply.github.com>
2023-06-23 16:56:47 +0530

    final commit

--- level1.py
+++ level1.py
@@ -1,25 +1,19 @@
 import json
-with open("C:\\Users\\jenis\\Downloads\\KLA\\Workshop_Problem\\Level_1_Input_Data\\input.json", 'r') as f:
-  data = json.load(f)
-print(data)
-
 from PIL import Image
-img1 = Image.open("C:\\Users\\jenis\\Downloads\\KLA\\Workshop_Problem\\Level_1_Input_Data\\wafer_image_1.png")
-#img1.show()
-
-img2 = Image.open("C:\\Users\\jenis\\Downloads\\KLA\\Workshop_Problem\\Level_1_Input_Data\\wafer_image_2.png")
-#img2.show()
-
-img3 = Image.open("C:\\Users\\jenis\\Downloads\\KLA\\Workshop_Problem\\Level_1_Input_Data\\wafer_image_3.png")
-#img3.show()
-
-img4 = Image.open("C:\\Users\\jenis\\Downloads\\KLA\\Workshop_Problem\\Level_1_Input_Data\\wafer_image_4.png")
-#img4.show()
-
-img5 = Image.open("C:\\Users\\jenis\\Downloads\\KLA\\Workshop_Problem\\Level_1_Input_Data\\wafer_image_5.png")
-#img5.show()
-
 from collections import Counter
+import csv
+
+def load_data_json(filepath):
+    with open(filepath, 'r') as f:
+        data = json.load(f)
+    return data
+
+def load_image(filepath):
+    img = Image.open(filepath)
+    return img
+
+def get_pixel_values(img):
+    return list(img.getdata())
 
 def find_majority_pixel(*lists, position):
     if len(set(map(len, lists))) != 1:
@@ -41,71 +35,42 @@ def compare_lists(list1,list2):
             y.append(i)
     return y
 
-pix_val1 = list(img1.getdata())
-pix_val2 = list(img2.getdata())
-pix_val3 = list(img3.getdata())
-pix_val4 = list(img4.getdata())
-pix_val5 = list(img5.getdata())
+def get_defective_pixels(pix_vals):
+    x=[]
+    for i in range(0,len(pix_vals[0])):
+        position = i
+        err =find_majority_pixel(*pix_vals, position=position)
+        x.append(err)
 
-x=[]
-for i in range(0,len(pix_val1)):
-    position = i
-    err =find_majority_pixel(pix_val1,pix_val2,pix_val3,pix_val4,pix_val5,position=position)
-    x.append(err)
+    err_lists = []
+    for i in range(len(pix_vals)):
+        err = compare_lists(x, pix_vals[i])
+        err_lists.append(err)
+    
+    x=[]
+    y=[]
+    rows = []
+    for i in range(len(err_lists)):
+        for j in range(len(err_lists[i])):
+            x1 = err_lists[i][j] % 800
+            y1 = 599 - err_lists[i][j] % 600
+            x.append(x1)
+            y.append(y1)
+            rows.append([i+1, x1, y1])
+    return rows
 
-err1 = compare_lists(x,pix_val1)
-err2 = compare_lists(x,pix_val2)
-err3 = compare_lists(x,pix_val3)
-err4 = compare_lists(x,pix_val4)
-err5 = compare_lists(x,pix_val5)
+def write_defects_csv(filename, rows):
+    with open(filename, 'w') as f:
+        write = csv.writer(f)
+        write.writerows(rows)
 
-x=[]
-y=[]
-for i in range(len(err1)):
-    x1 = err1[i]%800
-    y1= err1[i]%600
-    x.append(x1)
-    y.append(y1)
-rows =[]
-for i in range(0,len(x)):
-    rows.append([1,x[i],y[i]])
-
-for i in range(len(err2)):
-    x1 = err2[i] % 800
-    y1 = err2[i] % 600
-    x.append(x1)
-    y.append(y1)
-for i in range(0,len(x)):
-    rows.append([2,x[i],y[i]])
-
-for i in range(len(err3)):
-    x1 = err3[i] % 800
-    y1 = err3[i] % 600
-    x.append(x1)
-    y.append(y1)
-for i in range(0,len(x)):
-    rows.append([3,x[i],y[i]])
-
-for i in range(len(err4)):
-    x1 = err4[i] % 800
-    y1 = err4[i] % 600
-    x.append(x1)
-    y.append(y1)
-for i in range(0,len(x)):
-    rows.append([4,x[i],y[i]])
-
-for i in range(len(err5)):
-    x1 = err5[i] % 800
-    y1 = err5[i] % 600
-    x.append(x1)
-    y.append(y1)
-for i in range(len(err5)):
-    rows.append([5,x[i],y[i]])
-
-print(rows)
-
-import csv
-with open('defects', 'w') as f:
-    write = csv.writer(f)
-    write.writerows(rows)
-
+if __name__ == '__main__':
+    data = load_data_json("C:\\Users\\jenis\\Downloads\\KLA\\Workshop_Problem\\Level_1_Input_Data\\input.json")
+    pix_vals = []
+    image_paths = ["C:\\Users\\jenis\\Downloads\\KLA\\Workshop_Problem\\Level_1_Input_Data\\wafer_image_1.png","C:\\Users\\jenis\\Downloads\\KLA\\Workshop_Problem\\Level_1_Input_Data\\wafer_image_2.png","C:\\Users\\jenis\\Downloads\\KLA\\Workshop_Problem\\Level_1_Input_Data\\wafer_image_3.png","C:\\Users\\jenis\\Downloads\\KLA\\Workshop_Problem\\Level_1_Input_Data\\wafer_image_4.png","C:\\Users\\jenis\\Downloads\\KLA\\Workshop_Problem\\Level_1_Input_Data\\wafer_image_5.png"]
+    for path in image_paths:
+        img = load_image(path)
+        pix_vals.append(get_pixel_values(img))
+    
+    rows = get_defective_pixels(pix_vals)
+    write_defects_csv('defects', rows)
